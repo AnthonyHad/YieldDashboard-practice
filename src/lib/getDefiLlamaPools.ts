46b8cc03301633811f1ef9ee81c37dd8f92ca9ef
Anthony H <anthony.hadwan@hec.edu>
2025-05-21 18:46:25 +0200

    Added helper function to fetch pool apys from defillama

---
+++ src/lib/getDefiLlamaPools.ts
@@ -0,0 +1,38 @@
+export type Pool = {
+  chain: string;
+  project: string;
+  symbol: string;
+  tvlUsd: number;
+  apyBase: number;
+  apyReward: number | null;
+  apy: number;
+  rewardTokens: string;
+  apyPct1D: number;
+  apyPct7D: number;
+  apyPct30D: number;
+};
+
+export async function getDefiLlamaPools(): Promise<Pool[]> {
+  const response = await fetch("https://yields.llama.fi/pools");
+  if (!response.ok) {
+    throw new Error("Failed to fetch pools data");
+  }
+
+  const json = await response.json();
+
+  const pools = json.data.map((pool: Pool) => ({
+    chain: pool.chain,
+    project: pool.project,
+    symbol: pool.symbol,
+    tvlUsd: pool.tvlUsd,
+    apyBase: pool.apyBase,
+    apyReward: pool.apyReward,
+    apy: pool.apy,
+    rewardTokens: pool.rewardTokens,
+    apyPct1D: pool.apyPct1D,
+    apyPct7D: pool.apyPct7D,
+    apyPct30D: pool.apyPct30D,
+  }));
+
+  return pools;
+}
